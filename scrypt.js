b604137007819cce42573d8ff70e6878609aa9e1
Vanesa Edo <vanesa.en@gmail.com>
2024-02-13 10:47:27 +0100

    Incluidas variantes del Ejercicio 2 explicadas en clase

--- scrypt.js
+++ scrypt.js
@@ -14,7 +14,7 @@ console.log(tablaMultiplicar (5));*/
  
 //Ejercicio 2 : Pedir número al usuario
 
-/*const guardarNumeros = (introUserNum) => {
+const guardarNumeros = (introUserNum) => {
     let arrayFinal = [];    
   
     introUserNum = parseInt(window.prompt('Introduce un número'));
@@ -26,8 +26,42 @@ console.log(tablaMultiplicar (5));*/
 }  */  
 //.toSorted: Método que ordena el array de menor a mayor.
 /*(a, b) => (a - b) es una función que compara un número del array con el siguiente, para comprobar cuál es mayor. La escribo porque ordenaba considerando sólo el primer dígito de un número de dos dígitos. Por ejemplo: 1, 23, 4, ..Con esta función lo ordena así: 1, 4, 23..).*/
-//console.log(guardarNumeros().toSorted((a, b) => (a - b)));
+console.log(guardarNumeros().toSorted((a, b) => (a - b)));
 
+//E3V2 otra versión más simple por Alejandro
+
+function pedirNumeros(){
+
+    let numeros = [];
+    
+    while(true){ // 6 veces -> 0...5
+      const introUserNum = parseInt(prompt('Introduce un número'));
+      if(introUserNum == 0) break;
+      numeros.push(introUserNum);
+    }
+      return numeros.sort((a,b)=> a-b);
+    }
+
+//E3V3 Con Do While
+
+// While 0....N veces
+// Do-while 1....N veces
+
+//Ejercicio 2 : Pedir número al usuario
+const guardarNumeros = () => {
+    let arrayFinal = [];
+    let introUserNum = NaN;
+   do {
+      introUserNum = parseInt(prompt("Introduce un número")); //0
+      arrayFinal.push(introUserNum);
+        
+    } while (introUserNum != 0) 
+  
+    arrayFinal.pop(); // saca el 0 del final
+    return  arrayFinal.sort((a, b) => a - b); // ordena
+  };
+  
+  console.log(guardarNumeros());
 //Ejercicio 3. 
 
 //document.write(arrayFiunal.toSorted());
